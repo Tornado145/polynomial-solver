cc68642961748faa831358ddab5f2b455284e8ac
Tornado145 <varma.vatsal@gmail.com>
2016-12-14 12:14:08 -0500

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -4,12 +4,22 @@ public class Polynomial{
   private int[] coefficients;
   
   public Polynomial(int[] coefficients){
-  
+    this.coefficients = coefficients;
+    setValues(0);
   }
   
   public Polynomial(String polynomial){
-  
+    this.polynomial = polynomial;
+    setValues(1);
   }
   
+  private setValues(int methodRun){
+    if(methodRun == 0){
+      
+    } else if(methodRun == 1){
+    
+    }
+    
+  }
 
 }
