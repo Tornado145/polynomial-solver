2ba24662e5d7b03c88d126f36c74f0163e9fb796
Tornado145 <varma.vatsal@gmail.com>
2016-12-16 13:39:11 -0500

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -3,28 +3,13 @@ public class Polynomial{
   private int degree;
   private int[] coefficients;
   
-  public Polynomial(int[] coefficients){
-    this.coefficients = coefficients;
-    setDegree(coefficients);
-    setString();
-  }
-  
   public Polynomial(String polynomial){
     this.polynomial = polynomial;
     setArray();
   }
   
-  private: 
-    void setDegree(int[] coefficients){
-      degree = coefficients.length;  
-    }
-   
-    void setArray(){
-    
-    }
+  private void setArray(){
   
-    void setString(){
-    
-    }
   
+  }
 }
